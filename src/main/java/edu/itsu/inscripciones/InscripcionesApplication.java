27150b015aafbeffcbcf3a4f438de271322db2eb
vfacu44 <vfacu44@gmail.com>
2025-02-20 05:01:05 +0000

    first commit

---
+++ src/main/java/edu/itsu/inscripciones/InscripcionesApplication.java
@@ -0,0 +1,27 @@
+package edu.itsu.inscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class InscripcionesApplication {
+
+  @Value("${NAME:World}")
+  String name;
+
+  @RestController
+  class HelloworldController {
+    @GetMapping("/")
+    String hello() {
+      return "Hello " + name + "!";
+    }
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(InscripcionesApplication.class, args);
+  }
+
+}
