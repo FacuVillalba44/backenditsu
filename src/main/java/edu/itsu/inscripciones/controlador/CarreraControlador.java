d6554f50f7e9d817c7a9d66a49b31022272e03d7
vfacu44 <vfacu44@gmail.com>
2025-02-26 22:17:02 +0000

    feat: implementar CRUD básico de Carrera
    - Optimizado modelo Carrera.java con idPlanDeEstudio e inicioDeDictado
    - Añadido CarreraRepositorio.java para operaciones CRUD
    - Implementado ICarreraServicio.java y CarreraServicio.java con Optional
    - Agregado CarreraControlador.java con endpoints /itsuapi/carreras (GET, POST, PUT)

--- src/main/java/edu/itsu/inscripciones/controlador/CarreraControlador.java
+++ src/main/java/edu/itsu/inscripciones/controlador/CarreraControlador.java
@@ -1,11 +1,14 @@
 package edu.itsu.inscripciones.controlador;
+
 import java.util.List;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,33 +16,40 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.itsu.inscripciones.modelo.Carrera;
 import edu.itsu.inscripciones.servicio.CarreraServicio;
 
-
 @RestController
-// context-path = http://localhost:8080/itsuapi/carrera <- se vería así el url de la
+// context-path = http://localhost:8080/itsuapi/carrera <- se vería así el url
+// de la
 // api
-@RequestMapping("/itsuapi")//cambie el endpoint para que no quede redundante
+@RequestMapping("/itsuapi") // cambie el endpoint para que no quede redundante
 // Puerto desde donde recibira las peticiones del front
 @CrossOrigin(origins = "https://4200-idx-itsufront-1737645545390.cluster-vpxjqdstfzgs6qeiaf7rdlsqrc.cloudworkstations.dev")
 
-
 public class CarreraControlador {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(CarreraControlador.class);
-   
+
     @Autowired
     private CarreraServicio carreraServicio;
 
     // url donde se conectara es http://localhost:8080/api-itsu/carreras
-    @GetMapping("/carreras") //metodo para listar las carreras
+    @GetMapping("/carreras") // metodo para listar las carreras
     public List<Carrera> obtenerCarrera() {
         List<Carrera> carrera = this.carreraServicio.listarCarrera();
         logger.info("Obteniendo la lista de Carreras");
         carrera.forEach(carreraServicio -> logger.info(carrera.toString()));
         return carrera;
     }
+
     @PostMapping("/carreras")
-    public Carrera agregarCarrera(@RequestBody Carrera carrera){
-        logger.info("carrera a agregar:"+carrera);
+    public Carrera agregarCarrera(@RequestBody Carrera carrera) {
+        logger.info("carrera a agregar:" + carrera);
         return this.carreraServicio.guardarCarrera(carrera);
 
+    }
+
+    @PutMapping("/carreras/{id}")
+    public Carrera editarCarrera(@PathVariable Integer id, @RequestBody Carrera carrera) {
+        logger.info("Editando carrera con ID: " + id);
+        carrera.setIdCarrera(id);
+        return carreraServicio.guardarCarrera(carrera);
     }
 }
--- src/main/java/edu/itsu/inscripciones/servicio/ICarreraServicio.java
+++ src/main/java/edu/itsu/inscripciones/servicio/ICarreraServicio.java
@@ -1,12 +1,13 @@
 package edu.itsu.inscripciones.servicio;
 import java.util.List;
+import java.util.Optional;
 
 import edu.itsu.inscripciones.modelo.Carrera;
 
 public interface ICarreraServicio {
     public List<Carrera> listarCarrera();
-    public Carrera buscarCarreraPorId(Integer id);
-    //Carrera =  tipo de objeto que se va a guardar y carrera = valor que se va a guardar 
+    public Optional<Carrera> buscarCarreraPorId(Integer id);
+  
     public Carrera guardarCarrera(Carrera carrera); 
     public void eliminarCarrera(Integer idCarrera);
 }
